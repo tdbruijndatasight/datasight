fa043e21ae4950c8c137c0bed862c8b590655596
Tom de Bruijn <info.tomdebruijn@gmail.com>
2025-05-30 07:40:32 +0000

    for the splunk logo please use this SVG
    <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1255 1513" width="1255" height="1513">
    	<title>SPLK_BIG copy-svg</title>
    	<style>
    		.s0 { fill: #0c1724 }
    	</style>
    	<path id="Layer" fill-rule="evenodd" class="s0" d="m0.4 1512.9v-276l972.1-480.4-972.1-473.5v-282.9l1254.2 630.5v256.7z"/>
    </svg>

--- src/components/icons/custom-icons.tsx
+++ src/components/icons/custom-icons.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 
 // Splunk Icon
 export const SplunkIcon = ({ className }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M12 17.5l-4.5-4.5 4.5-4.5 4.5 4.5z" /> {/* Example: a diamond shape */}
-    <circle cx="12" cy="12" r="10" /> {/* Example: circle around it */}
+  <svg className={className} version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1255 1513" fill="currentColor" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+    <path id="Layer" fillRule="evenodd" d="m0.4 1512.9v-276l972.1-480.4-972.1-473.5v-282.9l1254.2 630.5v256.7z"/>
   </svg>
 );
 
 // Cribl Icon
 export const CriblIcon = ({ className }: { className?: string }) => (
-  <svg className={className} viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
-     <path d="M4 4 l16 8 l-16 8 Z" /> {/* Example: a different triangle */}
+  <svg className={className} width="38px" height="auto" fill="currentColor" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+     <path d="M15.7 8.9c.2.1.2.3 0 .4L.5 14.9c-.2.1-.4 0-.4-.2V.3C0 0 .2-.1.4.1zm3.6 2c-.3-.2-.6-.2-.9-.1L.5 17.5c-.3.1-.5.4-.5.7v1.1c0 .2.2.5.5.5h33.6c.3 0 .3-.1.1-.2z"/>
+     <path d="M.4 41.9c-.2.2-.4.1-.4-.2V27.2c.1-.2.3-.2.5-.2l15.2 5.7c.2.1.3.3 0 .4zM34.2 22.4c.2-.1.2-.2-.1-.2H.5c-.2 0-.5.2-.5.5v1.1c0 .3.2.6.5.7l17.9 6.7c.3.1.6.1.9-.1z"/>
   </svg>
 );
 
@@ -34,3 +34,4 @@ export const AiSolutionIcon = ({ className }: { className?: string }) => (
     <path d="M9 13v2" />
   </svg>
 );
+
