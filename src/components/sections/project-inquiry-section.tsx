7d18dc8393089a9f808fd0f9acfecf9b1cdc3bd8
Tom de Bruijn <info.tomdebruijn@gmail.com>
2025-06-02 13:23:24 +0000

    coffee cup a bit more to the right, and i want a part to be on the experience section

--- src/components/sections/project-inquiry-section.tsx
+++ src/components/sections/project-inquiry-section.tsx
@@ -166,7 +166,7 @@ const ProjectInquirySection: React.FC = () => {
   return (
     <section id="inquiry" className="bg-secondary section-min-height">
       <div className="container mx-auto relative">
-        <div className="absolute -top-24 right-8 transform -translate-y-1/2 z-10 hidden md:block pointer-events-none">
+        <div className="absolute -top-12 right-4 transform -translate-y-1/2 z-10 hidden md:block pointer-events-none">
           <Image
             src="/Coffee_Cup_DataSight.png"
             alt="Decorative coffee cup"
