f968a0d239ffe4aa6c84a12ff3c2db1582323f0b
Tom de Bruijn <info.tomdebruijn@gmail.com>
2025-06-03 07:06:39 +0000

    can you make sure the first and second coffee cup bot have -top-[4rem]
    Also the size is now 200 fixed, but i want this flexible based on screen side.
    
    On the mobile website I also want both coffee cups visible, the off cource centered like everything else, and a bit smaller so it does not take that much vertical room.

--- src/components/sections/contact-section.tsx
+++ src/components/sections/contact-section.tsx
@@ -60,14 +60,13 @@ const ContactSection: React.FC = () => {
 
   return (
     <footer id="contact" className="bg-gradient-to-br from-primary to-blue-700 text-primary-foreground py-16 md:py-24">
-      <div className="container mx-auto relative"> {/* Added relative positioning for the coffee cup */}
-        {/* Second Coffee Cup - Left Aligned */}
-        <div className="absolute -top-[4rem] left-0 transform -translate-y-1/2 z-10 hidden md:block pointer-events-none">
+      <div className="container mx-auto relative">
+        <div className="absolute -top-[4rem] transform -translate-y-1/2 z-10 pointer-events-none w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 left-1/2 -translate-x-1/2 md:right-auto md:left-0 md:translate-x-0">
             <Image
                 src="/Coffee_Cup_DataSight.png"
                 alt="Decorative coffee cup"
-                width={200}
-                height={200}
+                layout="fill"
+                objectFit="contain"
                 data-ai-hint="coffee cup illustration"
             />
         </div>
@@ -146,4 +145,3 @@ const ContactSection: React.FC = () => {
 };
 
 export default ContactSection;
-
--- src/components/sections/project-inquiry-section.tsx
+++ src/components/sections/project-inquiry-section.tsx
@@ -167,12 +167,12 @@ const ProjectInquirySection: React.FC = () => {
   return (
     <section id="inquiry" className="bg-secondary section-min-height">
       <div className="container mx-auto relative">
-        <div className="absolute -top-[3rem] right-0 transform -translate-y-1/2 z-10 hidden md:block pointer-events-none">
+        <div className="absolute -top-[4rem] transform -translate-y-1/2 z-10 pointer-events-none w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 left-1/2 -translate-x-1/2 md:left-auto md:right-0 md:translate-x-0">
           <Image
             src="/Coffee_Cup_DataSight.png"
             alt="Decorative coffee cup"
-            width={200}
-            height={200}
+            layout="fill"
+            objectFit="contain"
             data-ai-hint="coffee cup illustration"
           />
         </div>
