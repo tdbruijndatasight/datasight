9d53fad8266f86b34b739280e71a2ade418090b7
Tom de Bruijn <info.tomdebruijn@gmail.com>
2025-06-03 09:36:09 +0000

    de pen is goed, laat die nu zo staan, het koffiekopje is niet goed en moet nog hoger.

--- src/components/sections/project-inquiry-section.tsx
+++ src/components/sections/project-inquiry-section.tsx
@@ -168,12 +168,13 @@ const ProjectInquirySection: React.FC = () => {
     <section id="inquiry" className="bg-secondary section-min-height">
       <div className="container mx-auto relative">
         {/* Coffee Cup 1 */}
-        <div className="absolute -top-[4rem] transform -translate-y-1/2 z-10 pointer-events-none w-24 h-24 md:w-40 md:h-40 lg:w-48 lg:h-48 left-1/2 -translate-x-1/2 md:left-auto md:right-0 md:translate-x-0">
+        <div className="absolute -top-[5rem] transform -translate-y-1/2 z-10 pointer-events-none w-24 h-24 md:w-40 md:h-40 lg:w-48 lg:h-48 left-1/2 -translate-x-1/2 md:left-auto md:right-0 md:translate-x-0">
             <Image
                 src="/Coffee_Cup_DataSight.png"
                 alt="Decorative coffee cup"
                 layout="fill"
                 objectFit="contain"
+                
             />
         </div>
         <AnimatedSection className="text-center mb-12 relative z-20">
